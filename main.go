73a6f2849624418b3369414e26b14d56d7522eac
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-03-28 08:28:48 -0300

    Exercicio 1

--- main.go
+++ main.go
@@ -3,16 +3,29 @@ package main
 import "fmt" 
 
 func main() {
-	a, b := 10, 3
-	fmt.Println("A soma é", a + b)
-	fmt.Println("A subritração é", a - b)
-	fmt.Println("A multiplicação é", a * b)
-	fmt.Println("A divisão é", a / b)
-	fmt.Println("O resto da divisão é", a % b)
+	var num1, num2 float32
+	fmt.Print("Digite o primeiro número: ")
+	fmt.Scanln(&num1)
+	fmt.Print("Digite o segundo número: ")
+	fmt.Scanln(&num2)
 
-	a++
-	fmt.Println("Incrementar a", a)
-	if a > 0 && b > 0 {
-		fmt.Println("Numeros Positivos")
+	soma := num1 + num2
+	subtracao := num1 - num2
+	multiplicacao := num1 * num2
+	divisao := num1 / num2
+
+	fmt.Println("Resultados:")
+	fmt.Printf("Número 1: %.2f\n", num1)
+	fmt.Printf("Número 2: %.2f\n", num2)
+	fmt.Printf("Soma: %.2f + %.2f = %.2f\n", num1, num2, soma)
+	fmt.Printf("Subtração: %.2f - %.2f = %.2f\n", num1, num2, subtracao)
+	fmt.Printf("Multiplicação: %.2f * %.2f = %.2f\n", num1, num2, multiplicacao)
+	
+	
+	if num2 != 0 {
+		fmt.Printf("Divisão: %.2f / %.2f = %.2f\n", num1, num2, divisao)
+	} else {
+		fmt.Println("Divisão por zero não é permitida!")
 	}
 }
+
