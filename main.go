e34c11181b113790fc865ce3ec8e0372ea5bd492
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-03 09:18:49 -0300

    Slice

--- main.go
+++ main.go
@@ -4,10 +4,26 @@ import "fmt"
 
 func main (){
   var ages = [4]int{11,8,16,14}
-  nomes :=[4]string{"max", "maya", "mari", "miguel"}
+  nomes :=[4]string{"Rosa", "Amy", "Jack", "miguel"}
   fmt.Println(ages)
   fmt.Println(nomes)
-  
+//   slice
+var score = []int{100, 200, 300,}
+fmt.Println(score)
+rangeOne := score[1:3]
+fmt.Println(rangeOne)
+rangeTwo := score[2:]
+fmt.Println(rangeTwo)
+rangeTree := score[:3]
+fmt.Println(rangeTree)
+
+var superherois = []string{"Deadpool", "Homem Aranha", "Hulk", "Thor"}
+fmt.Println(superherois)
+superherois = append(superherois, "Homem de Ferro")
+fmt.Println(superherois, len(superherois), cap(superherois))
+
+
 }
+
 
 
