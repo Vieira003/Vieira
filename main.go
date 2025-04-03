b0eb834e26fd1d79440a24a2df3035f559a6a4f0
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-03 08:20:28 -0300

    Vetor 1

--- main.go
+++ main.go
@@ -2,30 +2,12 @@ package main
 
 import "fmt" 
 
-func main() {
-	var num1, num2 float32
-	fmt.Print("Digite o primeiro número: ")
-	fmt.Scanln(&num1)
-	fmt.Print("Digite o segundo número: ")
-	fmt.Scanln(&num2)
-
-	soma := num1 + num2
-	subtracao := num1 - num2
-	multiplicacao := num1 * num2
-	divisao := num1 / num2
-
-	fmt.Println("Resultados:")
-	fmt.Printf("Número 1: %.2f\n", num1)
-	fmt.Printf("Número 2: %.2f\n", num2)
-	fmt.Printf("Soma: %.2f + %.2f = %.2f\n", num1, num2, soma)
-	fmt.Printf("Subtração: %.2f - %.2f = %.2f\n", num1, num2, subtracao)
-	fmt.Printf("Multiplicação: %.2f * %.2f = %.2f\n", num1, num2, multiplicacao)
-	
-	
-	if num2 != 0 {
-		fmt.Printf("Divisão: %.2f / %.2f = %.2f\n", num1, num2, divisao)
-	} else {
-		fmt.Println("Divisão por zero não é permitida!")
-	}
+func main (){
+  var ages = [4]int{11,8,16,14}
+  nomes :=[4]string{"max", "maya", "mari", "miguel"}
+  fmt.Println(ages)
+  fmt.Println(nomes)
+  
 }
+
 
