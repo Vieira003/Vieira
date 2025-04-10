d261e20c68c7620dd1b2e8a36fccd269190f52a3
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-10 10:00:35 -0300

    Tarefa 10/04

--- main.go
+++ main.go
@@ -6,31 +6,16 @@ import (
 )
 
 func main() {
-    age := 16
-    fmt.Println(age <= 20)
-    fmt.Println(age >= 20)
-    fmt.Println(age == 20)
-    fmt.Println(age != 20)
-    if age < 20 {
-        fmt.Println("menor que 20 anos")
-    } else if age < 10 {
-        fmt.Println("menor que 10 anos")
-    } else {
-        fmt.Println("nao é menor que 20 anos")
+    var idade int
+    fmt.Println("qual sua idade?")
+    fmt.Scan(&idade)
+    if idade <18 {
+        fmt.Println("voce é menor de idade")
+    }else if idade > 18 && idade <= 60{
+        fmt.Println("voce é adulto")
+    }else {
+        fmt.Println("voce é idoso")
     }
-
-
-	names := []string{"Isadora", "Yasmim", "Martin", "Miguel", "Murilo"}
-
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("Continue após a posição", index, "Valor", value)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("sair após", index)
-			break
-		}
-		fmt.Println("Valor:", value)
-	}
+    
+    
 }
