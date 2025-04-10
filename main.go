ec07666579a387a6a5714d41905894d673b9a861
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-10 08:25:52 -0300

    Tarefa de Vetores

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"fmt" 
-	"strings"
-	"sort"
+	"fmt"
 )
 
-func main (){
-  greenting := "Hello my friends!"
-  fmt.Println(strings.Contains(greenting,"friends"))
-fmt.Println(strings.ReplaceAll(greenting, "Hello" , "Hi"))
-fmt.Println(strings.ToUpper(greenting))
-fmt.Println(strings.Index(greenting, "my"))
-fmt.Println(strings.Split(greenting, "friends"))
-ages := []int {50, 80 ,60}
-sort.Ints(ages)
-fmt.Println(ages)
-index := sort.SearchInts(ages, 50)
-fmt.Println(index)
-names := []string{"Caruzo", "Cris", "Julius"}
-sort.Strings(names)
-fmt.Println(names)
-fmt.Println(sort.SearchStrings(names, "Cris"))
+func main() {
+	var vetor [5]int
+	var soma int
 
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Digite o %dº número inteiro: ", i+1)
+		fmt.Scan(&vetor[i])
+		soma += vetor[i]
+	}
 
-
-
-
+	fmt.Printf("A soma dos números digitados é: %d\n", soma)
 }
-
-
-
