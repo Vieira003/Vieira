ffe4b285571bec927e5ce9e9c4dcaff00c77d5e8
Vieira003 <miguel.v.pereira@aluno.senai.br>
2025-04-17 10:54:58 -0300

    Tarefa 17/04

--- main.go
+++ main.go
@@ -1,54 +1,26 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
+func dadosPessoa(nome string, idade int) (int, string) {
+	var status string
+	if idade >= 18 {
+		status = "maior de idade"
+	} else {
+		status = "menor de idade"
+	}
+	return idade, status
+}
+
 func main() {
-    var saldo float32
-    fmt.Println("Insira o valor inicial:")
-    fmt.Scan(&saldo)
+	nome := "Miguel"
+	idade := 17
 
-    escolherOperacao(&saldo)
-}
+	idadeRetornada, status := dadosPessoa(nome, idade)
 
-func escolherOperacao(saldo *float32) {
-    var opcao int
-    fmt.Println("Você deseja sacar (1), depositar (2) ou encerrar a sessão (3)?")
-    fmt.Scan(&opcao)
-
-    switch opcao {
-    case 1:
-        sacar(saldo)
-    case 2:
-        depositar(saldo)
-    case 3:
-        encerrar()
-    default:
-        fmt.Println("Opção inválida.")
-    }
-}
-
-func depositar(saldo *float32) {
-    var valor float32
-    fmt.Println("Qual valor deseja depositar?")
-    fmt.Scan(&valor)
-    *saldo += valor
-    fmt.Println("Seu saldo atual é:", *saldo)
-}
-
-func sacar(saldo *float32) {
-    var valor float32
-    fmt.Println("Qual valor deseja sacar?")
-    fmt.Scan(&valor)
-    if valor > *saldo {
-        fmt.Println("Saldo insuficiente.")
-    } else {
-        *saldo -= valor
-        fmt.Println("Seu saldo atual é:", *saldo)
-    }
-}
-
-func encerrar() {
-    fmt.Println("Sessão encerrada.")
+	fmt.Println("Nome: ", nome )
+	fmt.Println("Idade: ", idadeRetornada)
+	fmt.Println("Status: ", status )
 }
